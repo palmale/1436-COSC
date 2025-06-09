7e7b50fdfff26f39ad1e698a02f5f135b2be5a78
palmale <palma.a014531@stu.sanjac.edu>
2025-06-09 03:42:06 +0000

    Updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,34 @@ public class HelloWorld
    public static void main(String [] args)
    {
         System.out.println("Hello, World");
+        
+        // Prints out my name
+        System.out.println("My name is Alessandra Palma");
+
+        // Prints out my favorite food
+        System.out.println("My favorite food is Sushi");
+
+        // Prints out a celebrity or publc figure I would like to meet 
+
+        System.out.println("The person I would've liked to meet is Princess Diana ");
+
+        //Prints out my least favorite chore
+
+        System.out.println("My least favorite chore is doing laundry.");
+
+        // Prints out what I would rather be doing
+
+        System.out.println("I would rather be watching TV.");
    } 
 }
+
+/*
+ *  The main() method is the entry point of the program. 
+ *    
+ *  System.out.println() is used to display the input as a text output to the terminal.
+ * 
+ * Comments are used to explain what the code does. It helps oneself and others communicate any questions or changes without messing up the code or getting in the way when the code is ran
+ *   
+ */
+
+
